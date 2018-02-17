913694ef21db7ceb7352f9184d01d02108776980
nisanurbulut <nisanurrunasin@gmail.com>
2018-02-17 13:25:47 +0300

    detail nesnesi için gereken sınıf yazıldı

---
+++ CrystalReport-MasterDetail/SalesInvoiceDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalReport_MasterDetail
+{
+  public  class SalesInvoiceDetail
+    {
+        public string InvoiceNo { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
