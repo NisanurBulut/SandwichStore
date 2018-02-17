486916c5a050314dd61a9e9f1949bedafc947103
nisanurbulut <nisanurrunasin@gmail.com>
2018-02-17 13:11:06 +0300

    master bilgileri taşıyacak olan nesneler için sınıf yazıldı

---
+++ CrystalReport-MasterDetail/SalesInvoiceHeader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalReport_MasterDetail
+{
+   public class SalesInvoiceHeader
+    {
+        public string InvoiceNo { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
