ec1d2e9347da0792097bdafad6ad170ad9fb666d
NisanurBulut <nbulut@sarplojistik.com>
2021-02-23 16:22:58 +0300

    implemented build controls component

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './BuildControls.css';
+const BuildControls=(props)=>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BuildControls;
