8a7650f94c8bb1495f39bc4c79ba3cbb64b90198
NisanurBulut <nbulut@sarplojistik.com>
2021-02-24 18:22:04 +0300

    deleted pizza builder and designed Sandwich builder

--- src/components/Sandwich/Sandwich.js
+++ src/components/Sandwich/Sandwich.js
@@ -2,17 +2,9 @@ import React from 'react';
 import './Sandwich.css';
 import SandwichIngredient from '../SandwichIngredient/SandwichIngredient';
 const Sandwich = (props) => {
-  function getToppingPosition(toppingRadius){
-    var posX, posY;
-    posX = -400-(Math.floor(Math.random() * ((325 * 2) - 1)));
-    posY = -100+Math.floor(Math.random() * ((125 * 2) - 1));
-    return { x: posX+'px', y: posY+'px' }
- }
   let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
-        var position=getToppingPosition(-15);
-        console.log(position);
         return <SandwichIngredient key={igKey + i} type={igKey} />;
       });
     })
