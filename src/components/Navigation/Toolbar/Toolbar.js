62f26471e826486d797231c8096c4485be1f6820
NisanurBulut <nbulut@sarplojistik.com>
2021-02-26 13:24:32 +0300

    created Toolbar component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Toolbar=()=>{
+    return (
+        <header>
+           <div>Menü</div>
+           <div>Logo</div>
+           <nav>
+               <ul>
+                   <li>
+                       Links
+                   </li>
+               </ul>
+           </nav>
+        </header>
+    )
+}
+
+export default Toolbar
