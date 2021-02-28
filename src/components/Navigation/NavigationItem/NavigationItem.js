47e19c6f8e8653b42b0fe7ca6e3e2b493673bbe6
NisanurBulut <nbulut@sarplojistik.com>
2021-02-28 11:09:41 +0300

    deleted unused code line

--- src/components/Navigation/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './NavigationItem.module.css';
 
 const NavigationItem = (props) => {
-  console.log(props);
   return (
     <li className={classes.NavigationItem}>
       <a href={props.link} className={props.active ? classes.active : null}>
