3ce6228ce7885b185063cf20ecfb02828785b480
NisanurBulut <nbulut@sarplojistik.com>
2021-02-28 10:52:28 +0300

    improved performance with avoiding rerendering

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import CustomButton from '../UI/Button/CustomButton';
 import classes from './OrderSummary.module.css';
 
-const OrderSummary = (props) => {
-  console.log(props);
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends Component {
+  componentWillUpdate(){
+    console.log('Order summary will upda');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+      return (
+        <li key={igKey}>
+          <span className={classes.Label}>{igKey}</span> :{' '}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
     return (
-      <li key={igKey}>
-        <span className={classes.Label}>{igKey}</span> :{' '}
-        {props.ingredients[igKey]}
-      </li>
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious Sandwich with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p className={classes.priceLabel}>
+          total Cost: <strong>{this.props.price.toFixed(2)}</strong>
+        </p>
+        <div className={classes.btnGroup}>
+          <CustomButton btnType="Danger" clicked={this.props.purchaseCancelled}>
+            Cancel
+          </CustomButton>
+          <CustomButton btnType="Success" clicked={this.props.purchaseContinued}>
+            Continue
+          </CustomButton>
+        </div>
+      </Auxiliary>
     );
-  });
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious Sandwich with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p className={classes.priceLabel}>total Cost: <strong>{props.price.toFixed(2)}</strong></p>
-      <div className={classes.btnGroup}>
-        <CustomButton btnType="Danger" clicked={props.purchaseCancelled}>Cancel</CustomButton>
-        <CustomButton btnType="Success" clicked={props.purchaseContinued}>Continue</CustomButton>
-      </div>
-    </Auxiliary>
-  );
-};
+  }
+}
 
 export default OrderSummary;
--- src/components/UI/GeneralModal/GeneralModal.js
+++ src/components/UI/GeneralModal/GeneralModal.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './GeneralModal.module.css';
-import Auxiliary  from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../BackDrop/Backdrop';
 
-const GeneralModal = (props) => {
-  return (
-    <Auxiliary>
-      <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
-    </Auxiliary>
-  );
-};
+class GeneralModal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  componentWillUpdate() {
+    console.log('Modal is updating');
+  }
+  render() {
+    return (
+      <Auxiliary>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </Auxiliary>
+    );
+  }
+}
 export default GeneralModal;
