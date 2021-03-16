b5ef87cb356b0f14d35578f334e32c4ef7e61b28
NisanurBulut <nbulut@sarplojistik.com>
2021-03-16 22:18:33 +0300

    converted class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import SandwichBuilder from './containers/SandwichBuilder/SandwichBuilder';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <SandwichBuilder />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Layout><SandwichBuilder /></Layout>;
+  }
 }
+
 
 export default App;
