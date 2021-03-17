d11cd2039299b4a97ecc56abab2af5170d6a8634
NisanurBulut <nbulut@sarplojistik.com>
2021-03-17 19:41:23 +0300

    implemented reducer for ingredient

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,35 @@
 import * as actionTypes from './actions';
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    meat: 0,
+    bacon: 0,
+    cheese: 0,
+  },
   totalPrice: 0,
 };
 
-const reducer = (state = initialState, action) => {};
-
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [acton.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+        return {
+            ...state,
+            ingredients: {
+              ...state.ingredients,
+              [acton.ingredientName]: state.ingredients[action.ingredientName] - 1,
+            },
+          };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
