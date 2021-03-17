d70e32c7d1ba02725a845a1d8909a762d56115b4
NisanurBulut <nbulut@sarplojistik.com>
2021-03-17 18:51:15 +0300

    styled order ingredient list

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Grid, Card } from 'semantic-ui-react';
+import { Card, List, Icon, ListItem } from 'semantic-ui-react';
 import classes from './Order.module.css';
 
-function Order() {
+function Order(props) {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    if (props.ingredients[ingredientName] > 0) {
+      ingredients.push({
+        amount: props.ingredients[ingredientName],
+        name: ingredientName,
+      });
+    }
+  }
+  const ingredientOutput = ingredients.map((ig) => {
+    return (
+      <ListItem key={ig.name}>
+        <List.Icon name="food" size="large" verticalAlign="middle" />
+        <List.Content>
+          <List.Header>{ig.name}</List.Header>
+          <List.Description>{ig.amount} Piece</List.Description>
+        </List.Content>
+      </ListItem>
+    );
+  });
   return (
     <div className={classes.Order}>
-       <Card>
-           <Card.Content>
-               <Card.Header>test</Card.Header>
-               <Card.Meta>5TL</Card.Meta>
-               <Card.Description>Description</Card.Description>
-           </Card.Content>
-       </Card>
+      <Card>
+        <Card.Content>
+          <Card.Header>Price: {Number.parseFloat(props.price)} ₺</Card.Header>
+          <Card.Meta></Card.Meta>
+          <Card.Description>
+            <List divided relaxed>
+              {ingredientOutput}
+            </List>
+          </Card.Description>
+        </Card.Content>
+      </Card>
     </div>
   );
 }
