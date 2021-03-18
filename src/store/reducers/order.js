2a30b71cfd66b1789716a9a1a12217c344ff44d3
NisanurBulut <nbulut@sarplojistik.com>
2021-03-18 17:05:34 +0300

    implemented order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  orders: [],
+  loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_SANDWICH_SUCCESS:
+      const newData = {
+        ...action.orderData,
+        id: action.orderId,
+      };
+      return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat(newData),
+      };
+    case actionTypes.PURCHASE_SANDWICH_FAIL:
+      return {
+          ...state,
+          loading:false,
+      };
+    default:
+      return state;
+  }
+};
