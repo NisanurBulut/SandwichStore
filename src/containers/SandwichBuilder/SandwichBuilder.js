b67747e3947c0908a78dc0baf94bd9ac3636a978
NisanurBulut <nbulut@sarplojistik.com>
2021-03-18 12:44:35 +0300

    implemented order state globally

--- src/containers/SandwichBuilder/SandwichBuilder.js
+++ src/containers/SandwichBuilder/SandwichBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions';
 
 class SandwichBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -41,7 +40,7 @@ class SandwichBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+   return sum > 0;
   }
   purchaseCancelHandler = () => {
     this.setState({ purchasing: false });
@@ -93,7 +92,7 @@ class SandwichBuilder extends Component {
                 ingredientRemoved={this.props.onIngredientRemoved}
                 disabled={disabledInfo}
                 price={this.props.price}
-                purchasable={this.state.purchasable}
+                purchasable={this.updatePurchaseState(this.props.ings)}
                 ordered={this.purchaseHandler}
               />
             </div>
