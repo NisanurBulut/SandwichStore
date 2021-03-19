5909c6c179aba0d4a4495e564720304a0f665fce
NisanurBulut <nbulut@sarplojistik.com>
2021-03-19 20:37:42 +0300

    refactored reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   orders: [],
@@ -9,42 +10,22 @@ const initialState = {
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_SANDWICH_START:
-      return {
-        ...state,
-        loading: true,
-      };
+      return updateObject(state, { loading: true });
     case actionTypes.PURCHASE_SANDWICH_SUCCESS:
-      const newData = {
-        ...action.orderData,
-        id: action.orderId,
-      };
-      return {
-        ...state,
+      const newData = updateObject(action.orderData, { id: action.orderId });
+      return updateObject(state, {
         loading: false,
         purchased: true,
         orders: state.orders.concat(newData),
-      };
+      });
     case actionTypes.PURCHASE_SANDWICH_FAIL:
-      return {
-        ...state,
-        loading: false,
-      };
-      case actionTypes.FETCH_ORDERS_START:
-        return {
-          ...state,
-          loading: true,
-        };
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-          return {
-            ...state,
-            orders:action.orders,
-            loading: false,
-          };
-        case actionTypes.FETCH_ORDERS_FAIL:
-          return {
-            ...state,
-            loading: false,
-          };
+      return updateObject(state, { loading: false });
+    case actionTypes.FETCH_ORDERS_START:
+      return updateObject(state, { loading: true });
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return updateObject(state, { orders: action.orders, loading: false });
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return updateObject(state, { loading: false });
     default:
       return state;
   }
