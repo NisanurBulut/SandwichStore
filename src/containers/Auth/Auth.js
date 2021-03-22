96db0ef0dc5c6b25bccf7a7449c67aed837280ac
NisanurBulut <nbulut@sarplojistik.com>
2021-03-22 15:14:55 +0300

    defined submit login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 import imageLogo from '../../assests/images/sanwich-logo.PNG';
 export class Auth extends Component {
+
+  loginHandler= (event) => {
+    event.preventDefault();
+    const loginData ={
+      email:event.target.elements.email.value,
+      password:event.target.elements.password.value
+    }
+    console.log(loginData);
+  }
     render() {
         const LoginForm =  (
             <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
@@ -9,15 +18,16 @@ export class Auth extends Component {
                 <Header as='h2' color='red' textAlign='center'>
                   <Image src={imageLogo} /> Log-in to your account
                 </Header>
-                <Form size='large'>
+                <Form size='large' onSubmit={this.loginHandler}>
                   <Segment stacked>
-                    <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                    <Form.Input fluid name="email" icon='user' iconPosition='left' placeholder='E-mail address' />
                     <Form.Input
                       fluid
                       icon='lock'
                       iconPosition='left'
                       placeholder='Password'
                       type='password'
+                      name="password"
                     />
 
                     <Button color='red' fluid size='large'>
