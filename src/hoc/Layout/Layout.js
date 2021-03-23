6d6f63b0249e30127558a4ce53a9858ba99c6fcd
NisanurBulut <nbulut@sarplojistik.com>
2021-03-23 15:49:30 +0300

    continue container store

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 import { Container } from 'semantic-ui-react';
+
 const Layout = (props) => {
   return (
     <Auxiliary>
@@ -11,5 +13,9 @@ const Layout = (props) => {
     </Auxiliary>
   );
 };
-
-export default Layout;
+const mapStateToProps = state=>{
+  return {
+    isAuthenticated:state
+  }
+}
+export default connect(Layout);
