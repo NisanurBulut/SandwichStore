bc123e06b76880759c34e45e9292e24886f35b7f
NisanurBulut <nbulut@sarplojistik.com>
2021-03-23 18:15:45 +0300

    controlled authenticated

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import {
   Button,
@@ -38,58 +39,68 @@ export class Auth extends Component {
           <Header as="h2" color="red" textAlign="center">
             <Image src={imageLogo} /> Log-in to your account
           </Header>
-         {
-          this.props.loading == true ? <Spinner /> :
-          <Form size="large">
-          <Segment stacked>
-            <Form.Input
-              fluid
-              name="email"
-              icon="user"
-              iconPosition="left"
-              placeholder="E-mail address"
-              value={this.state.email}
-              onChange={(e) => this.setState({ email: e.target.value })}
-              required
-            />
-            <Form.Input
-              fluid
-              icon="lock"
-              iconPosition="left"
-              placeholder="Password"
-              type="password"
-              name="password"
-              required
-              value={this.state.password}
-              onChange={(e) => this.setState({ password: e.target.value })}
-            />
+          {this.props.loading == true ? (
+            <Spinner />
+          ) : (
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  name="email"
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-mail address"
+                  value={this.state.email}
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                  required
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  required
+                  value={this.state.password}
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                />
 
-            <Button
-              color="red"
-              fluid
-              size="large"
-              onClick={this.loginHandler}
-              type="submit"
-            >
-              Login
-            </Button>
-          </Segment>
-        </Form>
-         }
+                <Button
+                  color="red"
+                  fluid
+                  size="large"
+                  onClick={this.loginHandler}
+                  type="submit"
+                >
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+          )}
           <Message>
             New to us? <a onClick={this.registerHandler}>Sign Up</a>
           </Message>
-          {this.props.error ? <Message color="red" size="small">{this.props.error.message}</Message>:null}
+          {this.props.error ? (
+            <Message color="red" size="small">
+              {this.props.error.message}
+            </Message>
+          ) : null}
         </Grid.Column>
       </Grid>
     );
-    return <div>{LoginForm}</div>;
+    if (this.props.state.isAuthenticated) {
+      <Redirect to="/" />;
+    } else {
+      return <div>{LoginForm}</div>;
+    }
   }
 }
 const mapStateToProps = (state) => {
   return {
     loading: state.auth.loading,
-    error: state.auth.error
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null,
   };
 };
 const mapDispatchToProps = (dispatch) => {
